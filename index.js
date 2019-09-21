067532af79bb23a862a0fa89b360c7ef576c4511
unknown <ya3ya63@gmail.com>
2019-09-21 18:52:51 +0430

    commit3

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const express = require('express')
+const app = express()
   var soap = require('soap');
   //var url = 'https://www.zarinpal.com/pg/services/WebGate/wsdl';
   //var url = 'http://127.0.0.1:8000/wsdl?wsdl';
@@ -16,11 +18,13 @@
                 'sCepOrigem':'11060000',
                 'sCepDestino':'11070000'
             }
+app.get('/', (req, res) => {
   soap.createClient(url, function(err, client) {
       //client.PaymentRequest(args, function(err, result) {
       client.CalcPrazo(args3, function(err, result) {
           logj(err);
-          logj(result);
+          res.send(result);
       });
-  });
+  });	
+})
   function logj(a){return console.log(JSON.stringify(a, null, 2))}
